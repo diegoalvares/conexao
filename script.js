361f1cb2e287f174c65df3e4a37af95e529b1f59
Diego Alvares de Macedo <101260437+diegoalvares@users.noreply.github.com>
2023-09-20 13:27:05 -0300

    atualizando e criando o js.

---
+++ script.js
@@ -0,0 +1,58 @@
+const btn = document.querySelector('#enviar');
+const nome = document.getElementById('nome')
+const senha = document.getElementById('senha')
+const msg = document.querySelector('h2')
+
+btn.addEventListener('click', function(e){
+    e.preventDefault();
+    const result = nome.value;
+    const result2 = senha.value;
+    console.log(result)
+    console.log(result2)
+
+    if (result == 'diego' && result2 == '051094'){
+        console.log('Login Efetuado com sucesso!')
+        msg.innerHTML = 'Login Efetuado com sucesso!'
+        msg.style.fontSize = '18px'
+    }
+    else if (result == 'juliana' && result2 == 1809){
+        console.log('Login Efetuado com sucesso!')
+        msg.innerHTML = 'Login Efetuado com sucesso!'
+        msg.style.fontSize = '18px'
+    }
+    else if (result == 'ajsouza' && result2 == 24879504){
+        console.log('Login Efetuado com sucesso!')
+        msg.innerHTML = 'Login Efetuado com sucesso!'
+        msg.style.fontSize = '18px'
+    }
+    else if (result == 'eu' && result2 == '981058@'){
+        console.log('Login Efetuado com sucesso!')
+        msg.innerHTML = 'Login Efetuado com sucesso!'
+        msg.style.fontSize = '18px'
+    }
+    else if (result == 'josifono' && result2 == 'androli2027'){
+        console.log('Login Efetuado com sucesso!')
+        msg.innerHTML = 'Login Efetuado com sucesso!'
+        msg.style.fontSize = '18px'
+    }
+    else if (result == 'josifono' && result2 == 'androli2027'){
+        console.log('Login Efetuado com sucesso!')
+        msg.innerHTML = 'Login Efetuado com sucesso!'
+        msg.style.fontSize = '18px'
+    }
+    else if (result == 'biaaprincesa' && result2 == 'migoocupado'){
+        console.log('Login Efetuado com sucesso!')
+        msg.innerHTML = 'Login Efetuado com sucesso!'
+        msg.style.fontSize = '18px'
+    }
+    else if (result == '' || result2 == ''){
+        console.error('Preencha todos os campos!')
+        msg.innerHTML = 'Preencha todos os campos!'
+        msg.style.fontSize = '19px'
+    }
+    
+    else{
+        console.error('Usuário ou senha incorreta!')
+        msg.innerHTML = 'Usuário ou senha incorreta!'
+    }
+})
