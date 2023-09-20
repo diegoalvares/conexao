900f2d481e3f0f816138f6d05c09473d73199e5a
Diego Alvares de Macedo <101260437+diegoalvares@users.noreply.github.com>
2023-09-20 15:06:56 -0300

    começando a criar a tela de perfil.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const btn = document.querySelector('#enviar');
+const btn = document.querySelector('#entrar');
 const nome = document.getElementById('nome')
 const senha = document.getElementById('senha')
 const msg = document.querySelector('h2')
@@ -11,39 +11,46 @@ btn.addEventListener('click', function(e){
     console.log(result2)
 
     if (result == 'diego' && result2 == '051094'){
-        console.log('Login Efetuado com sucesso!')
-        msg.innerHTML = 'Login Efetuado com sucesso!'
-        msg.style.fontSize = '18px'
+        console.log('Login Efetuado com sucesso!');
+        msg.innerHTML = 'Login Efetuado com sucesso!';
+        msg.style.fontSize = '18px';
+        location.href='perfil.html';
     }
     else if (result == 'juliana' && result2 == 1809){
         console.log('Login Efetuado com sucesso!')
         msg.innerHTML = 'Login Efetuado com sucesso!'
         msg.style.fontSize = '18px'
+        location.href='perfil.html';
     }
     else if (result == 'ajsouza' && result2 == 24879504){
         console.log('Login Efetuado com sucesso!')
         msg.innerHTML = 'Login Efetuado com sucesso!'
         msg.style.fontSize = '18px'
+        location.href='perfil.html';
     }
     else if (result == 'eu' && result2 == '981058@'){
         console.log('Login Efetuado com sucesso!')
         msg.innerHTML = 'Login Efetuado com sucesso!'
         msg.style.fontSize = '18px'
+        location.href='perfil.html';
     }
     else if (result == 'josifono' && result2 == 'androli2027'){
         console.log('Login Efetuado com sucesso!')
         msg.innerHTML = 'Login Efetuado com sucesso!'
         msg.style.fontSize = '18px'
+        location.href='perfil.html';
     }
     else if (result == 'josifono' && result2 == 'androli2027'){
         console.log('Login Efetuado com sucesso!')
         msg.innerHTML = 'Login Efetuado com sucesso!'
         msg.style.fontSize = '18px'
+        location.href='perfil.html';
     }
     else if (result == 'biaaprincesa' && result2 == 'migoocupado'){
         console.log('Login Efetuado com sucesso!')
         msg.innerHTML = 'Login Efetuado com sucesso!'
         msg.style.fontSize = '18px'
+        location.href='perfil.html';
     }
     else if (result == '' || result2 == ''){
         console.error('Preencha todos os campos!')
