15407f44f3d8728f74a7ede8118b3c129d20bf67
Diego Alvares de Macedo <101260437+diegoalvares@users.noreply.github.com>
2023-09-22 17:56:43 -0300

    atualizando

--- nav.js
+++ nav.js
@@ -8,7 +8,6 @@ roxo.addEventListener('click', function(e){
     e.preventDefault();
     const fundo = document.querySelector('body')
     fundo.style.background = 'linear-gradient(to right, rgb(90, 5, 90) 55%, rgb(142, 62, 150))'
-    animacao();
 })
 
 
@@ -16,14 +15,12 @@ preto.addEventListener('click', function(e){
     e.preventDefault();
     const fundo = document.querySelector('body')
     fundo.style.background = 'linear-gradient(to right, rgb(31, 30, 30) 60%, rgb(56, 54, 54)'
-    animacao();
 })
 
 azul.addEventListener('click', function(e){
     e.preventDefault();
     const fundo = document.querySelector('body')
     fundo.style.background = 'linear-gradient(to right, rgb(5, 82, 128) 45%,  rgb(10, 109, 109)'
-    animacao();
 })
 
 
@@ -31,11 +28,4 @@ rosa.addEventListener('click', function(e){
     e.preventDefault();
     const fundo = document.querySelector('body')
     fundo.style.background = 'linear-gradient(to right, rgb(255, 0, 221),rgb(255, 103, 217) 55%, white)'
-    animacao();
 })
-
-
-function animacao(){
-    return resultado.animate([
-    ], {duration: 500})
-};
